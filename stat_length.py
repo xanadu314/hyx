b12ef7e8477047916715102dc67ca8e0d076a8c3
xanadu314 <37304315+xanadu314@users.noreply.github.com>
2019-04-15 13:41:49 +0800

    Add files via upload
    PS E:\> python .\stat_length.py M7.FA
    >scaffold116    94783
    >scaffold117    134339
    >scaffold114    2062
    >scaffold115    88747
    >scaffold112    13176
    >scaffold113    172331
    >scaffold110    250943
    >scaffold111    23340
    >scaffold118    12174
    >scaffold119    58917
    >scaffold29     7967

---
+++ stat_length.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+import sys,os,re
+
+
+def process_file(reader):
+    '''Open, read,and print a file'''
+    names=[]
+    index=0
+    dict={}
+
+
+    for line in reader:
+        if line.startswith('>'):
+           if index >=1:
+               names.append(line)
+           index =index+1
+           name=line[:-1]
+           seq = ''
+        else:
+           seq +=line[:-1]
+           dict[name]=seq
+    return dict
+
+
+if __name__ == "__main__":
+    input_file=open(sys.argv[1],"r")
+    reader=input_file.readlines()
+    items=process_file(reader)
+    for key in items:
+        length=int(len(items[key]))
+        print "%s\t%d" %(key,length)
+    input_file.close()
